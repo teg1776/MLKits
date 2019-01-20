7d1a81066adb93f254d4a8440565f8c956da66d5
teg1776 <teg1776@gmail.com>
2019-01-19 18:13:29 -0600

    playing around with k to see what gives optimal results

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,44 @@
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const testSetSize = 100;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+  _.range(1, 15).forEach(k => {
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value();
+    console.log("For k of:", k, "Accuracy:", accuracy);
+  });
 }
 
+function knn(data, point, k) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .parseInt()
+    .value();
+}
+
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+}
